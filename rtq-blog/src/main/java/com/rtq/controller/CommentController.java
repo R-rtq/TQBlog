990ffbae0f4da132901c5818bd9ac182ef7bfbdb
rentianqi <2246509327@qq.com>
2023-01-04 16:55:06 +0800

    使用Swagger2实现接口文档，用户前后端项目接口对接编写，展示详情参数及信息。

--- rtq-blog/src/main/java/com/rtq/controller/CommentController.java
+++ rtq-blog/src/main/java/com/rtq/controller/CommentController.java
@@ -2,8 +2,14 @@ package com.rtq.controller;
 
 import com.rtq.constants.SystemConstants;
 import com.rtq.domain.ResponseResult;
+import com.rtq.domain.dto.AddCommentDto;
 import com.rtq.domain.entity.Comment;
 import com.rtq.service.CommentService;
+import com.rtq.utils.BeanCopyUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
  **/
 @RestController
 @RequestMapping("/comment")
+@Api(tags = "评论",description = "评论相关接口")
 public class CommentController {
 
 
@@ -27,11 +34,17 @@ public class CommentController {
     }
     //友链评论列表
     @GetMapping("/linkCommentList")
+    @ApiOperation(value = "友链评论列表",notes = "获取一页友链评论")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum",value = "页码"),
+            @ApiImplicitParam(name = "pageSize",value = "每页大小")
+    })
     public ResponseResult linkCommentList(Integer pageNum,Integer pageSize){
         return commentService.commentList(SystemConstants.LINK_COMMENT,null,pageNum,pageSize);
     }
     @PostMapping
-    public ResponseResult addComment(@RequestBody Comment comment){
+    public ResponseResult addComment(@RequestBody AddCommentDto addCommentDto){
+        Comment comment = BeanCopyUtils.copyBean(addCommentDto, Comment.class);
         return commentService.addComment(comment);
     }
 }
